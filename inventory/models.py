76297640b0f35a0490b55906954145f46ac05312
ziaalian <ziadhalian@gmail.com>
2022-10-18 20:18:52 +0200

    adding catgories,store using vanila django

--- inventory/models.py
+++ inventory/models.py
@@ -1,13 +1,32 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Store(models.Model):
+    name = name = models.CharField(max_length=200)
+    address = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    store_category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     store = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    phone = models.IntegerField()
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
+    store = models.ForeignKey('Store', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
